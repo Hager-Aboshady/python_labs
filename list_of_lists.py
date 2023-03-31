1fae2bb8906feffc01c993a17eb3b101bb5ca868
Hager <hageraboshady1@gmail.com>
2023-03-31 07:59:36 +0200

    sixth lab is done

---
+++ list_of_lists.py
@@ -0,0 +1,13 @@
+num = int(input("Enter a number: "))
+
+list_of_lists = []
+
+for i in range(num):
+    
+    list = []  #to create a new list
+    
+    for j in range(i+1):
+        list.append((j+1)*(i+1))
+    list_of_lists.append(list)
+    
+print(list_of_lists)
