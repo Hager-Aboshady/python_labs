7fd1bc12ced3ad5b9e1a84b8aa1ef8e70e0f1305
Hager <hageraboshady1@gmail.com>
2023-03-31 23:56:33 +0200

    1st lab is donee

---
+++ day2/fun_with_2_args.py
@@ -0,0 +1,14 @@
+list=[]
+def myfun (length,start):
+    num=start
+    for i in range(length):
+        list.append(num)
+        num=num+1
+
+
+l=int(input("please enter the length of you list : "))
+s=int(input("please enter the number you want to start from : "))
+
+myfun(l,s)
+print("your list is: ",list)
+        
