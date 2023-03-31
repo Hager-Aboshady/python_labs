c30d40fa1767e71792bd547feddca57cbc85eff3
Hager <hageraboshady1@gmail.com>
2023-04-01 00:11:37 +0200

    2nd lab is done

--- day2/fizz_buzz_function.py
+++ day2/fizz_buzz_function.py
@@ -1,10 +1,10 @@
 def myfun (num):
-    if(num%3==0):
-        print("FIZZ")
+    if(num%3==0 and num%5==0):
+        print("FIZZ BUZZ")
     elif(num%5==0):
         print("BUZZ")
-    elif(num%3==0 and num%5==0):
-        print("FIZZ BUZZ") 
+    elif(num%3==0):
+        print("FIZZ") 
     else:
         print("This Number is not divisable by 3 nor 5 ")    
 
@@ -12,5 +12,5 @@ def myfun (num):
 
 num=int(input("Please Enter a Number : "))
 
-
+myfun(num)
 
