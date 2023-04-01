cd1e2844e48ae8bb16e4297c99ba0cc1d896e68a
Hager <hageraboshady1@gmail.com>
2023-04-01 02:07:13 +0200

    editing

--- day2/fizz_buzz_function.py
+++ day2/fizz_buzz_function.py
@@ -1,16 +1,20 @@
 def myfun (num):
-    if(num%3==0 and num%5==0):
+    
+     if(num%3==0 and num%5==0):
         print("FIZZ BUZZ")
-    elif(num%5==0):
+     elif(num%5==0):
         print("BUZZ")
-    elif(num%3==0):
+     elif(num%3==0):
         print("FIZZ") 
-    else:
-        print("This Number is not divisable by 3 nor 5 ")    
+     else:
+        print("This Number is not divisable by 3 nor 5 ")  
+       
 
 
 
 num=int(input("Please Enter a Number : "))
-
 myfun(num)
+ 
+
+
 
---
+++ day2/reversing_function.py
@@ -0,0 +1,10 @@
+def revFun(word):
+    return word[::-1]
+
+
+word=input("Please Enter a Word : ")
+
+revWord=revFun(word)
+print(f"Your Word Before Reversing is {word} and After Reversing is {revWord} ")
+
+
