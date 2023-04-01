5a35b6d86cd05c4f9ce6cf33ffc36d4ec4938795
Hager <hageraboshady1@gmail.com>
2023-04-01 21:44:18 +0200

    in progress

--- hangman_game.py
+++ hangman_game.py
@@ -7,14 +7,19 @@ print(f"Welcome {name} to Hangman Game !")
 
 words_list=['apple','elephant,','pizza','banana','hello','world','goat','tiger']
 
-secret_word=random.choice(words_list)
-	#Choose a random item from a sequence. Here seq can be a list, tuple, string, or any iterable like range.
+
+secret_word=random.choice(words_list)                   #Choose a random item from a the list 
+	
 
 turns=7
+print("The Secret word consists of  ",end='')         #we assign end='' to print in the same line in Python 
+    
+
 
 for i in range(len(secret_word)):
-    print("_")
-    
+
+    print(" _ ", end='')
+
+print(" ")    
 #while turns<=7:
     
-
