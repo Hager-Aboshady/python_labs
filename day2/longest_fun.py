08825ad32839ddd15d6f23180f969defc2a1ceb2
Hager <hageraboshady1@gmail.com>
2023-04-02 06:56:08 +0200

    edit is done

--- day2/longest_fun.py
+++ day2/longest_fun.py
@@ -1,48 +1,32 @@
-# def longest_sub(string):
-
-#     longest=''
-#     cur_sub=''
-
-#     for i in range(len(string)):
-#         if (i==0,string[i]>= string[i-1]):
-#             cur_sub+= string[i]
-
-#         else:
-#             if (len(longest) < len (cur_sub)) :
-            
-#                 longest=cur_sub
-#                 cur_sub+=string[i]
-
-#     if len(cur_sub) > len(longest):
-#         longest = cur_sub
-
-#     print("Longest substring in alphabetical order is:", longest)           
-
-      
-# string=input("Please Enter any String you want :  ")
-
-# longest_sub(string)
-
-
 def get_longest_letters(string):
 
     letters=[]
     longest_letters=[]
 
-
     for i in range(len(string)):
-        if(i==0 or string[i]>=string[i-1]):
+        if(i==0 or string[i] >= string[i-1]): 
             letters.append(string[i])
-            if(len(letters)>=len(longest_letters)):
-                longest_letters=letters
+
         else:
-            continue        
-    print ("The longest One is :: ",longest_letters)
+
+            if len(letters) > len(longest_letters):   #check that  if the current list is the longest one and if so set it to longest
+                 longest_letters = letters              
+            letters=[]              #reset the list (because the order is different )to contain the  current letter  and start  again
+            letters.append(string[i])
+
+    if(len(letters)>=len(longest_letters)):
+            longest_letters=letters 
+
+    separator = ''
+    output_letters = separator.join(longest_letters)
+    print("The longest One is  : ",output_letters) 
+
+  #  print ("The longest One is :: ",longest_letters)    #will print it as  a list 
         
 
-s=input("Please Enter any String you want :  ")
+    s=input("Please Enter any String you want :  ")
 
-get_longest_letters(s)
+    get_longest_letters(s)
 
 
 
