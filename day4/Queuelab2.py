4cf23d5eba61ab5e2a737d0af528ff24d3775a95
Hager <hageraboshady1@gmail.com>
2023-05-03 04:18:28 +0300

    save and load class methods have been added

--- day4/Queuelab2.py
+++ day4/Queuelab2.py
@@ -5,7 +5,8 @@ class Queue:
         self.name = name
         self.myList = []
         self.size = size
-        Queue.objs[name] = self  #set the obj in the dictionary #the key will be the name and the value is the obj
+        Queue.objs[name] = self  #set the obj in the dictionary 
+                                 #the key will be the name and the value is the obj
 
     def insert(self, value):
         num_items=len(self.myList)
@@ -26,24 +27,51 @@ class Queue:
 
     def is_empty(self):
         if len(self.myList) == 0:
-            # print("The Queue is Empty ")
+            ## print("The Queue is Empty ")
             return True
         else:
-            # print(f"The Queue is Not Empty and The number of its items is {len(self.myList)} ")
+            ## print(f"The Queue is Not Empty and The number of its items is {len(self.myList)} ")
             return False    
 
     @classmethod
     def get_obj(cls, name):
         return cls.objs.get(name, "NOT FOUND")
+    @classmethod
+    def save_objs(cls):
+        fileObJ=open("instances.txt","w")
+        fileObJ.write(str(cls.objs))
+        
+    
+
+    @classmethod
+    def load_objs(cls):
+        fileObj=open("instances.txt","r")
+        print(fileObj.readlines())
+
     
 q=Queue("obj1",3)
 q.insert(1)
 q.insert(2)
 
-# print(q.get_obj("obj1"))
-# print(q.get_obj("obj2"))
+print(Queue.get_obj("obj1"))
+print(Queue.get_obj("obj2"))
 
 q.insert(3)
+Queue.save_objs()
 #q.insert(4)
 """-------------------------------------"""
+q2=Queue("obj2",3)
+
+Queue.save_objs()
+
+q3=Queue("obj3",3)
+
+Queue.save_objs()
+Queue.load_objs()
+
+#print(Queue.get_obj("obj2"))
+
+
+
+
 
